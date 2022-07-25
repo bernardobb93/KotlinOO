50bb3ec249e6e2ddf47a9501e46e5b5377e8dfbc
bernardobb <bernardobb.eletronica@gmail.com>
2022-07-25 11:21:07 -0300

    kotlin OO

--- src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -3,9 +3,11 @@ package one.digitalinnovation.digionebank
 class Pessoa {
     var nome:String="Bernardo"
     var cpf: String="123.123.123-05"
+    private set
 }
 fun main(){
     val Bernardo=Pessoa()
     println(Bernardo.nome)
     println(Bernardo.cpf)
+
 }
