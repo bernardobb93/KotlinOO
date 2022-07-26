5e83f3239bb5b31ec9183499b95dcd8b8db095e7
bernardobb <bernardobb.eletronica@gmail.com>
2022-07-26 09:10:22 -0300

    kotlin OO

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/ClienteTipoEnumTeste.kt
@@ -0,0 +1,13 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.ClienteTipo
+
+fun main(){
+    ClienteTipo.values().forEach { elemento ->
+        println("${elemento.name} - ${elemento.descricao}")
+    }
+    val pf = ClienteTipo.PF
+    println("${pf.name} - ${pf.descricao}")
+    val pj = ClienteTipo.PJ
+    println("${pj.name} - ${pj.descricao}")
+}
