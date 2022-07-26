ad85fc2d8b248c4c35c1da1563accaa97cf61e40
bernardobb <bernardobb.eletronica@gmail.com>
2022-07-26 08:46:08 -0300

    kotlin OO

---
+++ src/main/kotlin/one/digitalinnovation/digionebank/Banco.kt
@@ -0,0 +1,6 @@
+package one.digitalinnovation.digionebank
+
+data class Banco(
+    val nomeB:String,
+    val numero:Int
+)
---
+++ src/main/kotlin/one/digitalinnovation/digionebank/testes/BancoTeste.kt
@@ -0,0 +1,9 @@
+package one.digitalinnovation.digionebank.testes
+
+import one.digitalinnovation.digionebank.Banco
+
+fun main(){
+    val digiOneBank = Banco("DigiOneBank",12)
+    println(digiOneBank.nomeB)
+    println(digiOneBank.numero)
+}
